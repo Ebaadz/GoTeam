effd817f45c260c01588617a3e1a4bcfc3922ad2
wenzhenw <90291967+wenzhenw@users.noreply.github.com>
2024-12-02 23:16:29 -0500

    Update Player.java

--- Project Final/BlackJack-GoTeam/src/blackjack/base/Player.java
+++ Project Final/BlackJack-GoTeam/src/blackjack/base/Player.java
@@ -4,60 +4,65 @@
  */
 package blackjack.base;
 
+
 import java.util.ArrayList;
 import java.util.List;
-/**
- *
- * @author GoTeam
- */
-public class Player {
-    private String name;
-    private List<Card> hand;
-    private int score;
 
-    // creates a player with a given name
+public class Player {
+    private String name; // Player's or dealer's name
+    private List<Card> hand; // Cards the player or dealer holds
+
+    // Constructor
     public Player(String name) {
         this.name = name;
         this.hand = new ArrayList<>();
-        this.score = 0;
     }
 
+    // Set the player's name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Get the player's name
+    public String getName() {
+        return name;
+    }
+
+    // Add a card to the player's hand
     public void addCard(Card card) {
         hand.add(card);
-        score = calcScore(); //calls method to calculate the score
-    } //check score after each usage of addCard in the main method to see if game ends
-    
-    public int calcScore() {
-        int hand_score = 0;
-        int aces = 0; //track how many aces are in the hand
-        for (Card c : hand) {
-            hand_score += c.getValue();
-            if ("A".equals(c.getRank())) {
-                aces += 1;
+    }
+
+    // Get the player's current score based on their hand
+    public int getScore() {
+        int score = 0;
+        int aceCount = 0;
+
+        // Calculate score considering Aces as either 1 or 11
+        for (Card card : hand) {
+            score += card.getValue();
+            if (card.getRank().equals("Ace")) {
+                aceCount++;
             }
         }
-        //if score exceeds 21, check if there are any aces and subtract
-        //10 points for each ace
-        while (aces > 0 && hand_score > 21) {
-            hand_score -= 10;
-            aces -= 1;
-        }
-        return hand_score;
-    }
-    
-    public String displayHand() {
-        String cards_in_hand = "";
-        for (Card c : hand) {
-            cards_in_hand += c.getRank() + " of " + c.getSuit() + "\n";
-        }
-        return cards_in_hand;
-    }
-    
-    public int getScore() { return score; }
-    public String getName() { return name; }
 
+        // Adjust for Aces if necessary
+        while (score > 21 && aceCount > 0) {
+            score -= 10; // Count Ace as 1 instead of 11
+            aceCount--;
+        }
+
+        return score;
+    }
+
+    // Get the hand of cards
+    public List<Card> getHand() {
+        return hand;
+    }
+
+    // Return the string representation of the hand
     @Override
     public String toString() {
-        return name + "'s hand: " + hand + " | Score: " + score;
+        return name + "'s hand: " + hand + " | Score: " + getScore();
     }
 }
